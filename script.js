e09a61123584d6fdfa6cf671214e2587ab90e351
Тимур Мурадян <muradyan@sig.local>
2022-05-21 20:39:15 +0300

    created first homework

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+// let c = "Hello, World!!";
+// alert(c);
+
+// let userName = prompt("Ваше имя?");
+// alert("Hello, " + userName);
+
+// let a = 5;
+// let b = 4;
+
+// alert(a + b);
+// alert(a - b);
+// alert(a / b);
+// alert(a * b);
+// alert(a ** b);
+// alert(a % b);
+
+// let name = "John";
+
+// if (name === "John") {
+//   console.log("Hi");
+// } else {
+//   console.log("Good Bye");
+// }
+
+let hour = 12;
+if (hour < 10 || hour > 18) {
+  console.log("Офис закрыт");
+} else if (hour > 10 && hour < 18) {
+  console.log("Офис открыт");
+}
